cc4780de3707f2b80fbada0a005ec6dfb98043b3
Alpha Olomi <alphaolomi@gmail.com>
2024-03-14 12:45:42 +0300

    Update FileUtilTest.php

--- tests/Feature/Utils/FileUtilTest.php
+++ tests/Feature/Utils/FileUtilTest.php
@@ -16,7 +16,7 @@ test('generate file name with correct extension', function () {
     // expect($fileName)->toMatch('/^[0-9a-z]{26}\.pdf$/');
     // is string
     expect($fileName)->toBeString();
-})->markTestIncomplete('Need to fix the test');
+});
 
 test('generate file name with extension from MIME type when original extension is missing', function () {
     $file = UploadedFile::fake()->create('image', 100, 'image/jpeg'); // Intentionally missing extension
@@ -26,7 +26,7 @@ test('generate file name with extension from MIME type when original extension i
     // expect($fileName)->toMatch('/^[0-9a-z]{26}\.jpeg$/');
     // is string
     expect($fileName)->toBeString();
-})->markTestIncomplete('Need to fix the test');
+});
 
 test('generate slug from filename', function () {
     $filename = 'example.document.pdf';
