6ab05a292a122b540190a545394a05e292ff6ca8
Alpha Olomi <alphaolomi@gmail.com>
2024-03-15 12:24:10 +0300

    Refactor exception handling in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->reportable(function (Throwable $e) {
+            // api/* routes will return JSON
+            if (request()->is('api/*')) {
+                return response()->json(['error' => $e->getMessage()], 500);
+            }
+        });
     })->create();
